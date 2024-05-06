3bed32458d8a86e391751ab5edf488eab0196c9d
Jordan Kisiel <jordan.kisiel@gmail.com>
2024-05-05 20:44:34 -0400

    Fixed color config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,30 +7,32 @@ const config: Config = {
         "./app/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
-        red: {
-            100: "#F1D7D7",
-            400: "#EE7C7D",
-        },
-        orange: {
-            100: "#F0E1D7",
-            400: "#F5AC7F",
-        },
-        blue: {
-            100: "#E2ECEF",
-            400: "#61C4E1",
-        },
-        pink: {
-            100: "#F1E0E8",
-            400: "#D973A3",
-        },
-        purple: {
-            100: "#E4DEED",
-            400: "#8054C4",
-        },
-        neutral: {
-            100: "#ECE5E1",
-            300: "E0E0E0",
-            900: "#161616",
+        colors: {
+            red: {
+                100: "#F1D7D7",
+                400: "#EE7C7D",
+            },
+            orange: {
+                100: "#F0E1D7",
+                400: "#F5AC7F",
+            },
+            blue: {
+                100: "#E2ECEF",
+                400: "#61C4E1",
+            },
+            pink: {
+                100: "#F1E0E8",
+                400: "#D973A3",
+            },
+            purple: {
+                100: "#E4DEED",
+                400: "#8054C4",
+            },
+            neutral: {
+                100: "#ECE5E1",
+                300: "E0E0E0",
+                900: "#161616",
+            },
         },
     },
     plugins: [],
