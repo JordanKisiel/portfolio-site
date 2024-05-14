9e5ee479aafe36df25a7d62db3dbb8cef54c3db2
Jordan Kisiel <jordan.kisiel@gmail.com>
2024-05-13 21:57:49 -0400

    Updated CroppedImage component to allow for height and color overlay styling

--- components/CroppedImage.tsx
+++ components/CroppedImage.tsx
@@ -1,19 +1,28 @@
-import { StaticImport } from "next/dist/shared/lib/get-img-props"
-
 type Props = {
-    image: string | StaticImport
+    image: string
     imageSize: string
     imagePosition: string
+    height: string
+    colorOverlay?: string
 }
 
 export default function CroppedImage({
     image,
     imageSize,
     imagePosition,
+    height,
+    colorOverlay,
 }: Props) {
     return (
-        <div
-            className={`rounded-[5px] overflow-hidden w-full h-48 mb-[0.625rem] ${image} ${imageSize} ${imagePosition}`}
-        ></div>
+        <div className="relative">
+            {colorOverlay && (
+                <div
+                    className={`absolute inset-0 z-10 mix-blend-screen opacity-50 ${colorOverlay}`}
+                ></div>
+            )}
+            <div
+                className={`rounded-[5px] overflow-hidden w-full mb-[0.625rem] bg-no-repeat ${image} ${imageSize} ${imagePosition} ${height}`}
+            ></div>
+        </div>
     )
 }
