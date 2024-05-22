278f6d35fddc3a229cca8e57a160786c6bebdec7
Jordan Kisiel <jordan.kisiel@gmail.com>
2024-05-21 23:15:53 -0400

    Fixed Photo component issue on mobile

--- components/Photo.tsx
+++ components/Photo.tsx
@@ -3,18 +3,10 @@ import picOfMe from "@/public/pic-of-me.png"
 
 export default function Photo() {
     return (
-        <div className="rounded-full overflow-hidden border-[6px] border-neutral-900 drop-shadow-[6px_6px_0px_rgba(22,22,22,1)]">
-            <div className="absolute inset w-full h-full bg-gradient-to-r from-orange-400 via-blue-400 to-pink-400 to-50% opacity-75 mix-blend-hue z-10"></div>
-            <Image
-                className="absolute inset-0 blur-[10px] opacity-50 mix-blend-darken"
-                src={picOfMe}
-                alt="Photograph of Jordan Kisiel's face"
-                priority={true}
-            />
-            <Image
-                src={picOfMe}
-                alt="Photograph of Jordan Kisiel"
-            />
-        </div>
+        <div
+            className="relative rounded-full border-[6px] bg-no-repeat bg-[url('../public/pic-of-me.png')] 
+                     border-neutral-900 drop-shadow-[6px_6px_0px_rgba(22,22,22,1)] w-[8rem] bg- aspect-square bg-[-5rem_-0.5rem]
+                     bg-[length:15rem]"
+        ></div>
     )
 }
